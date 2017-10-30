3a9c071e1e0c32554f6bb25ac4888ab9abc90cf6
John Iacona <john.iacona@gmail.com>
2017-10-30 20:09:45 +0000

    replaced copied code with useless code

--- twitter/server.rb
+++ twitter/server.rb
@@ -1,37 +1,9 @@
 require 'sinatra'
-require 'pony'
 
 get '/' do
   erb :form
 end
 
-post '/' do
-  @email = params[:email]
-  @from_email = params[:from]
-  @subject = params[:subject]
-  @message = params[:message]
-
-  Pony.options= {
-    :via => 'smtp',
-    :via_options => {
-      :address => 'smtp.mailgun.org',
-      :port => '587',
-      :enable_starttls_auto => true,
-      :authentication => :plain,
-      :user_name => '<your username here>',
-      :password => '<your password here>',
-    }
-  }
-
-  message = {
-    :from => @from_email,
-    :to => @email,
-    :subject => @subject,
-    :body => @message
-  }
-
-  Pony.mail(message)
-
-  erb :result
+get '/something' do
+  'something really exciting'
 end
-
